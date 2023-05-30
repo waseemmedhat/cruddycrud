c8e8aa479409b602062d38e2d0dcb7eb8728a6a2
waseemmedhat <waseem.medhat@clin-pro.com>
2023-05-30 21:12:02 +0300

    feat: title section

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
-    import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="min-h-screen bg-yellow-200">
+	<div class="container mx-auto p-10">
+		<slot />
+	</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-center text-5xl font-bold leading-loose tracking-widest">CruddyCrud</h1>
+<h2 class="text-center text-2xl font-bold tracking-wide">Yet another CRUD app</h2>
+<h3 class="text text-center font-bold tracking-wide">(... in case it's not obvious)</h3>
